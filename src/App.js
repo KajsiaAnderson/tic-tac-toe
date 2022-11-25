2f63ba521f217364ad01dbe1cbbc6c8b62954d1f
Kajsia Anderson <kajsia.anderson@gmail.com>
2022-11-25 15:40:50 -0700

    added .map to component and handled user click events

--- src/App.js
+++ src/App.js
@@ -6,13 +6,27 @@ function App() {
   const [squares, setSquares] = useState(["", "", "", "", "", "", "", "", "",])
   const [player, setPlayer] = useState(true)
 
+  const handleClick = () => {
+    setSquares(["", "", "", "", "", "", "", "", "",])
+    setPlayer(true)
+  }
+
   return (
     <div className="App">
-      <Square
-        squares={squares}
-        setSquares={setSquares}
-        player={player}
-        setPlayer={setPlayer} />
+      <button onClick={handleClick}>Reset</button>
+      <div className="container">
+        {squares.map((value, index) => {
+          return (
+            <Square
+              squares={squares}
+              setSquares={setSquares}
+              player={player}
+              setPlayer={setPlayer}
+              squareValue={value}
+              index={index} />
+          )
+        })}
+      </div>
     </div>
   );
 }
--- src/Square.jsx
+++ src/Square.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 
 function Square(props) {
+    function handleClick() {
+        if (!props.squareValue) {
+            if (props.player) {
+                props.squares.splice(props.index, 1, "X");
+                props.setSquares(props.squares);
+                props.setPlayer(!props.player);
+            } else {
+                props.squares.splice(props.index, 1, "O");
+                props.setSquares(props.squares);
+                props.setPlayer(!props.player);
+            }
+        }
+    }
     return (
-        console.log(props.squares, props.player)
-        // <div className='square'>
-        // </div>
+        // console.log(props.squares, props.player)
+        <div className='square' onClick={handleClick}>
+            {props.squareValue === "O" ? <img src="https://cdn.discordapp.com/attachments/830137099042816080/984895322184634448/devcircle_1.png" alt="Devmountain circle" /> : props.squareValue}
+        </div>
     )
 }
 
